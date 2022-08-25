029fe9a6c496d0b90d28f122bfd0120b6cffe5bb
WappleLord <80618053+WappleLord@users.noreply.github.com>
2022-08-25 09:06:59 +0800

    Add files via upload

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,3 +1,8 @@
-function a(){  
-    alert("IDK");  
-   }  
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
